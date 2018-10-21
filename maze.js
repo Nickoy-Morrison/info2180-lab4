dd9931629532693090fe30543107dc2e01f62d2d
[Nickoy-Morrison] <nickoy.pv2014@gmail.com>
2018-10-21 13:53:13 -0500

    finish exercise 1

---
+++ maze.js
@@ -0,0 +1,14 @@
+window.onload = main;
+var flag = 0;
+function main(){
+	//Exercise 1
+		document.getElementById("boundary1").addEventListener("mouseover", part1);
+
+
+
+}
+
+//Exercise 1
+function part1(){
+		this.setAttribute("class","boundary youlose");
+}

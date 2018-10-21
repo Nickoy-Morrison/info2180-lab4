fd75ba43e5395b7e27c75794f86a737264c18503
[Nickoy-Morrison] <nickoy.pv2014@gmail.com>
2018-10-21 14:19:29 -0500

    finish exercise 2

--- maze.js
+++ maze.js
@@ -1,14 +1,28 @@
 window.onload = main;
 var flag = 0;
 function main(){
-	//Exercise 1
+		//Exercise 1
 		document.getElementById("boundary1").addEventListener("mouseover", part1);
 
-
+		//Exercise 2
+		var boundary = document.querySelectorAll(".boundary");
+		for (var i = 0; i < boundary.length-1; i++) {
+				boundary[i].addEventListener("mouseover", part2);
+		}
 
 }
 
 //Exercise 1
 function part1(){
 		this.setAttribute("class","boundary youlose");
+}
+
+//Exercise 2
+function part2(){
+	var boundary = document.querySelectorAll(".boundary");
+	for(var i = 0; i < boundary.length; i++){
+			boundary[i].className = "boundary youlose";
+			flag = 1;
+	}
+	part5(flag);
 }
